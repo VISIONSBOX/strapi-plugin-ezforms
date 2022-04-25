b8ae989c2605a5394622cc7cc3dac7504782db8c
Fabian Kunde <fabian.kunde@visionsbox.de>
2022-04-25 10:32:08 +0200

    Ein bisschen schönere Mail und replyTo sender

--- server/services/notification-providers/email.js
+++ server/services/notification-providers/email.js
@@ -3,18 +3,21 @@ module.exports = ({strapi}) => ({
   async send(config, data){
     let recipients = await strapi.query('plugin::ezforms.recipient').findMany();
     let message = ""
+
     //Loop through data and construct message from data object
     for(let key in data){
-      message += `${key}: ${data[key]}\n`
+      message += `<strong>${key}:</strong> ${data[key]}<br/>`
     }
     //loop through the recipients and send an email
+    // https://docs.strapi.io/developer-docs/latest/plugins/email.html#programmatic-usage
     for(let recipient of recipients){
       try{
         await strapi.plugins['email'].services.email.send({
           to: recipient.email,
           from: config.from,
-          subject: 'New Contact Form Submission',
-          text: message,
+          replyTo: data.email,
+          subject: 'Neue Anfrage über das Kontakformular',
+          html: message,
         });
       } catch (e) {
         strapi.log.error(e)
